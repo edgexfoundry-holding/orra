2df535c6e50c4bbf2d4de51bda4a4e7d3843d9fa
Anthony Casagrande <anthony.j.casagrande@intel.com>
2022-03-14 14:20:03 -0700

    feat: add ability to comment json files
    - publish device onvif to local registry
    
    Signed-off-by: Anthony Casagrande <anthony.j.casagrande@intel.com>

---
+++ demos/OH-EXF-Jakarta/README.md
@@ -0,0 +1,7 @@
+
+## Publish
+
+Run a local registry
+```shell
+docker run -d -p 5000:5000 --restart=always --name registry registry:2
+```
--- demos/OH-EXF-Jakarta/publish/make/service-tmpl.mk
+++ demos/OH-EXF-Jakarta/publish/make/service-tmpl.mk
@@ -9,21 +9,33 @@ export SERVICE_NAME ?= $(EDGE_OWNER).$(EDGE_DEPLOY).$(IMAGE_NAME)
 export SERVICE_VERSION ?= $(call version_of,$(IMAGE_NAME))
 export DOCKER_IMAGE_VERSION ?= $(SERVICE_VERSION)$(VERSION_SUFFIX)
 
+BUILD_DIR = ../../build
+SERVICE_JSON_INPUT = service.definition.json
+SERVICE_JSON_OUTPUT = $(BUILD_DIR)/$(IMAGE_NAME).service.json
+
 .PHONY: all publish-service deploy-policy print-service
 
 all: publish-service deploy-policy
 
 # Publish a Horizon service (per service.json) and pull image to get its sha256
-publish-service:
-	hzn exchange service publish -O -f service.definition.json --pull-image
+publish-service: $(SERVICE_JSON_OUTPUT)
+	hzn exchange service publish -O -f "$(SERVICE_JSON_OUTPUT)" --pull-image
 
-print-service:
-	envsubst < "service.definition.json"
+# Print out service.json file substituting env variables
+print-service: $(SERVICE_JSON_OUTPUT)
+	envsubst < "$(SERVICE_JSON_OUTPUT)"
 
 # Only add a deploy policy if one is specified in "service.policy.json"
 deploy-policy:
 ifneq ("$(wildcard service.policy.json)","")
 	hzn exchange deployment addpolicy -f service.policy.json deploy_$(EDGE_OWNER).$(EDGE_DEPLOY).$(IMAGE_NAME)_$(ARCH)
 endif
+
+$(BUILD_DIR):
+	mkdir -p "$@"
+
+# remove comments from pattern.json files (any line starting with //)
+$(SERVICE_JSON_OUTPUT): $(SERVICE_JSON_INPUT) | $(BUILD_DIR)
+	sed -E 's|^\s*//.*||g' "$(SERVICE_JSON_INPUT)" > "$@"
 
 endif # SERVICE_TMPL_MK_DEFINED
