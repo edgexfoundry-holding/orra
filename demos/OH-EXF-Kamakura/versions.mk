336e662a2429bde059ececb32ecab42cd4a0deb4
Anthony Casagrande <anthony.j.casagrande@intel.com>
2022-08-22 15:32:12 -0700

    feat: upgrade EVAM to 0.7.2
    Signed-off-by: Anthony Casagrande <anthony.j.casagrande@intel.com>

--- demos/OH-EXF-Kamakura/versions.mk
+++ demos/OH-EXF-Kamakura/versions.mk
@@ -4,12 +4,12 @@
 export VERSION_redis ?= 6.2.6
 export VERSION_consul ?= 1.10.10
 export VERSION_mqtt_broker ?= 2.0.12
-export VERSION_deploy_data ?= 0.1.0
+export VERSION_deploy_data ?= 2.2.0
 export VERSION_edge_video_analytics ?= 0.7.2
-export VERSION_model_data ?= 0.1.0
+export VERSION_model_data ?= 2.2.0
 export VERSION_device_onvif_camera ?= 2.2.0
-# because device-onvif camera is in nexus registry, it gets incremental build numbers
-export VERSION_SUFFIX_device_onvif_camera ?= -dev.9
+# if using device-onvif-camera from the nexus registry, use the incremental build numbers (-dev.9 etc.)
+export VERSION_SUFFIX_device_onvif_camera ?=
 
 #### EdgeX Core/Stock Service Versions ####
 # Format: VERSION_<service_name>, where service_name is the base name of the service
@@ -24,6 +24,6 @@ export VERSION_edgex_ui ?= $(EDGEX_CORE_VERSION)
 #### Pattern Versions ####
 # Format: VERSION_PATTERN_<pattern_name>, where pattern_name is the base name of the pattern,
 # 		  but with `-` converted to `_`.
-export VERSION_PATTERN_edgex_kamakura ?= 0.2.0
-export VERSION_PATTERN_edgex_kamakura_edge_video_analytics ?= 0.2.0
-export VERSION_PATTERN_edge_video_analytics ?= 0.2.0
+export VERSION_PATTERN_edgex_kamakura ?= 2.2.0
+export VERSION_PATTERN_edgex_kamakura_edge_video_analytics ?= 2.2.0
+export VERSION_PATTERN_edge_video_analytics ?= 2.2.0
